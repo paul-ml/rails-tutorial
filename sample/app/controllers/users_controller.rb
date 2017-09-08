97a79338b015330d00ac3f0b442ceaeaef05fbbf
Paul Joe George(JP_SMN) <31526130+paul-ml@users.noreply.github.com>
2017-09-08 13:50:34 +0900

    sign up

--- sample/app/controllers/users_controller.rb
+++ sample/app/controllers/users_controller.rb
@@ -1,4 +1,14 @@
 class UsersController < ApplicationController
+ 
   def new
+
+  	@user=Userr.new
   end
+
+  def show
+
+@user= Userr.find(params[:id])
+
+  end
+
 end
--- sample/config/routes.rb
+++ sample/config/routes.rb
@@ -7,7 +7,7 @@ get 'contact', to: 'static_pages#contact'
 get 'signup', to:'users#new'
   get 'home', to: 'static_pages#home'
 root'static_pages#home'
-
+resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
