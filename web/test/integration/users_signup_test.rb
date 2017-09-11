44f43323a22659c8c01cb338c82e9f444ce6f30d
Paul Joe George(JP_SMN) <31526130+paul-ml@users.noreply.github.com>
2017-09-11 13:58:11 +0900

    use ssl and puma

--- web/test/integration/users_signup_test.rb
+++ web/test/integration/users_signup_test.rb
@@ -14,15 +14,14 @@ assert_no_difference 'User.count' do
 	post users_path, params: {user: {name: "",email:"abcd@invalid",password: "abcd", password_confirmation: "efgh"}}
 end
 assert_template 'users/new'
- assert_select 'div#<CSS id for error explanation>'
-    assert_select 'div.<CSS class for field with error>'
+ 
 end
 
 
 
 
 test "valid SignUp" do
-get signup_path 
+get signup_path
 
 assert_difference 'User.count' ,1 do
 	 post users_path, params: {user: {name:"abcdefgh",email:"abcdefghj@gmail.com",password:"abcdefghtjk",password_confirmation:"abcdefghtjk"}}
