da4be9bd2550e5846ef1c5771660a92977c9fab3
Paul Joe George(JP_SMN) <31526130+paul-ml@users.noreply.github.com>
2017-09-12 13:35:23 +0900

    remember me added

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,9 @@ Rails.application.routes.draw do
   root'static_pages#home'
   resources :users
   post '/signup', to: 'users#create'
+  get '/login' , to: 'sessions#new'
+  post'login', to: 'sessions#create'
+  delete'logout', to: 'sessions#destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
